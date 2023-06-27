aed5a101e7adc3dded159854e20a0ec1806581a5
necklace <xiangxn@163.com>
2023-06-27 10:13:56 +0800

    update execute to executeTransaction

--- src/db/api/user.js
+++ src/db/api/user.js
@@ -1,4 +1,4 @@
-const { execute } = require("../pool");
+const { execute, executeTransaction } = require("../pool");
 
 async function getUnbindingUsers(limit = 100) {
     let sql = "SELECT * FROM twitter_auth_record WHERE status=1 ORDER BY create_time LIMIT ?;"
@@ -14,7 +14,7 @@ async function updateStatus(user, status) {
         sql += `INSERT INTO user_info (twitter_id,near_id,twitter_username,twitter_name,profile_img) 
                 VALUES ('${user.twitter_id}','${user.near_id}','${user.twitter_username}','${user.twitter_name}','${user.profile_img}');`
     }
-    await execute(sql, [status, user.twitter_id]);
+    await executeTransaction(sql, [status, user.twitter_id]);
 }
 
 module.exports = {
