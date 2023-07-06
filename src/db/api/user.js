b550d9d656a54d5da9d0bb7506f66ccc641f294a
necklace <xiangxn@163.com>
2023-07-06 16:26:40 +0800

    fix updateStatus bug

--- src/db/api/user.js
+++ src/db/api/user.js
@@ -12,9 +12,9 @@ async function updateStatus(user, status) {
     let sql = "UPDATE twitter_auth_record SET status=? WHERE twitter_id=?;";
     if (status == 2) {
         sql += `INSERT INTO user_info (twitter_id,near_id,twitter_username,twitter_name,profile_img) 
-                VALUES ('${user.twitter_id}','${user.near_id}','${user.twitter_username}','${user.twitter_name}','${user.profile_img}');`
+                VALUES (?,?,?,?,?);`
     }
-    await executeTransaction(sql, [status, user.twitter_id]);
+    await executeTransaction(sql, [status, user.twitter_id, user.twitter_id, user.near_id, user.twitter_username, user.twitter_name, user.profile_img]);
 }
 
 module.exports = {
